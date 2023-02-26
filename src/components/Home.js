4e05051d4e8f66caed1d90fc87328dd507c246d7
Zara Shipchandler <zarashipchandler@Zaras-MacBook-Air.local>
2023-02-26 13:58:39 -0600

    html for app

---
+++ src/components/Home.js
@@ -0,0 +1,55 @@
+
+import Card from 'react-bootstrap/Card';
+import Image from 'react-bootstrap/Image'
+import CardGroup from 'react-bootstrap/CardGroup';
+ 
+import { useState } from 'react';
+import Button from 'react-bootstrap/Button';
+import Form from 'react-bootstrap/Form';
+import 'bootstrap/dist/css/bootstrap.css';
+const {Configuration, OpenAIApi} = require("openai");
+
+
+
+function Home() {
+    const [header, setHeader] = useState("Response from AI:")
+    const [response, setResponse] = useState("... awaiting response")
+	return (
+        <div>
+            <br/>
+            <h1>Welcome to an AI Powered Generator!</h1>
+            <br/>
+            <Form>
+                <Form.Group className="mb-3" controlId="formBasicEmail">
+                    <Form.Label>Generate X here</Form.Label>
+                    <Form.Control type="text" name="description" placeholder="Enter description here" />
+                    <Form.Text className="text-muted">
+                    The more specific the description, the better the response.
+                    </Form.Text>
+                </Form.Group>
+
+                <Button variant="primary" size="lg" type="submit">
+                    Get AI Response
+                </Button>
+            </Form>
+            <br/>
+            <br/>
+
+            <Card>
+                <Card.Body>
+                    <Card.Title><h1>{header}</h1></Card.Title>
+                    <hr/>
+                    <br/>
+                    <Card.Text>
+                        <h4>
+                            {response}
+                        </h4>
+                    </Card.Text>
+                </Card.Body>
+            </Card>
+
+        </div>
+	);
+  }
+
+export default Home;
